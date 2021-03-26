6790aff6f850a54687a1715ab623b11a764c54de
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2021-03-26 23:44:40 +0100

    3. zadatak (1.)

--- app/src/main/java/com/example/cinaeste/mvvm/viewmodel/MovieListViewModel.kt
+++ app/src/main/java/com/example/cinaeste/mvvm/viewmodel/MovieListViewModel.kt
@@ -7,7 +7,7 @@ class MovieListViewModel {
     fun getFavoriteMovies():List<Movie>{
         return MovieRepository.getFavoriteMovies();
     }
-    fun getRecentMovies():List<Movie>{
+    fun getRecentMovies():List<Movie> {
         return MovieRepository.getRecentMovies();
     }
 }
