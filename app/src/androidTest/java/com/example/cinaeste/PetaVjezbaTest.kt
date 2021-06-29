be20cafb39d252fd8dc2d44d93cc311fec7bb049
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2021-06-29 15:24:38 +0200

    Vjezba 8

--- app/src/androidTest/java/com/example/cinaeste/PetaVjezbaTest.kt
+++ app/src/androidTest/java/com/example/cinaeste/PetaVjezbaTest.kt
@@ -1,14 +1,13 @@
 package com.example.cinaeste
 
 import android.content.Intent
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.cinaeste.view.SimpleStringAdapter
+import com.example.cinaeste.view.SimpleCastStringAdapter
 import com.example.cinaeste.viewmodel.MovieDetailViewModel
 import org.junit.Rule
 import org.junit.Test
@@ -24,11 +23,11 @@ class PetaVjezbaTest {
     @Test
     fun testActors(){
         val pokreniDetalje: Intent = Intent(MovieDetailActivity::javaClass.name)
-        pokreniDetalje.putExtra("movie_title","In Time")
+        pokreniDetalje.putExtra("movie_title","Pulp Fiction")
         detailRule.launchActivity(pokreniDetalje)
-        val actors = MovieDetailViewModel().getActorsByTitle("In Time")
+        val actors = MovieDetailViewModel().getActorsByTitle("Pulp Fiction")
         for(actor in actors)
-            onView(withId(R.id.listActors)).perform(RecyclerViewActions.scrollTo<SimpleStringAdapter.SimpleViewHolder>(
+            onView(withId(R.id.listActors)).perform(RecyclerViewActions.scrollTo<SimpleCastStringAdapter.SimpleViewHolder>(
                 withText(actor))).check(matches(isDisplayed()))
     }
     //Zadatak 2
